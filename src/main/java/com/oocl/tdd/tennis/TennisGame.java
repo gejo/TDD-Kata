7f171a41d1ce61723ab82b91dda7743e13a883b0
Joy Ge <joy.ge@oocl.com>
2014-08-07 14:53:44 +0800

    Add first failed test

---
+++ src/main/java/com/oocl/tdd/tennis/TennisGame.java
@@ -0,0 +1,10 @@
+package com.oocl.tdd.tennis;
+
+/**
+ * Created by GEJO on 8/7/2014.
+ */
+public class TennisGame {
+    public String getScore() {
+        return null;
+    }
+}
---
+++ src/test/java/TennisGameTest.java
@@ -0,0 +1,16 @@
+import com.oocl.tdd.tennis.TennisGame;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by GEJO on 8/7/2014.
+ */
+public class TennisGameTest {
+
+    @Test
+    public void start_game() {
+        TennisGame game = new TennisGame();
+        assertEquals("0:0", game.getScore());
+    }
+}
